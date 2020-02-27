9e950a12de93032d27e81bc953730af2bdefe4c6
AlexanderMikhaylov <a.a.mikhaylov2015@gmail.com>
2020-02-27 11:47:31 +0300

    OOP

---
+++ src/hw_5/Coworker.java
@@ -0,0 +1,18 @@
+package hw_5;
+
+public class Coworker {
+    private String name, position, email, telephone;
+    private int salary, age;
+
+    public Coworker(String name, String position, String email, String telephone, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void info(){
+        System.out.println("Name: " + name + ", position: " + position + ", email: " + email+ ", tel.: " + telephone + ", salary: " + salary + ", age: " + age);
+    }
+}
